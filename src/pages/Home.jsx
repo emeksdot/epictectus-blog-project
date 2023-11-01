da5387326f25056af03536b20c06f9cd52961b59
emeksdot <emeksdot@yahoo.com>
2023-11-01 20:58:38 +0100

    Changed the font to Patrick Hand

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  background-color: aqua;
+`;
+
+const Home = () => {
+  return <Container>Home</Container>;
+};
+
+export default Home;
